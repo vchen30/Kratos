4befd4eafd0b56405d186f5119497d0ce34cee74
Unknown <mmaso@cimne.upc.edu>
2018-03-09 16:50:13 +0100

    first routine contributions

--- applications/MeshingApplication/python_scripts/non_conforming_triangle_refinement_utility.py
+++ applications/MeshingApplication/python_scripts/non_conforming_triangle_refinement_utility.py
@@ -10,4 +10,24 @@ class NonConformingTriangleRefinementUtility:
         self.origin_model_part = origin_model_part
         self.destination_model_part = destination_model_part
     
+    def Initialize(self):
+        pass
     
+    def Execute(self):
+        self.destination_model_part.Nodes = self.origin_model_part.Nodes
+        nodes_hash = {}
+        for element in self.origin_model_part.Elements:
+            # print(element.__dir__())
+            # for i in range(3):
+            #     # print(nodes.__dir__())
+            #     new_node_hash = (element.GetNode(i).Id, element.GetNode(i+1).Id)
+            #     print(new_node_hash)
+            #     if not new_node_hash in nodes_hash:
+            #         self.destination_model_part.AddNode
+            #         nodes_hash[new_node_hash] = 1
+            
+            # First edge
+            new_node_hash = (element.GetNode(0).Id, element.GetNode(1).Id)
+            if not new_node_hash in nodes_hash:
+                # self.destination_model_part.AddNode()  # Añadir el nodo en el punto medio
+                # nodes_hash[new_node_hash] = 1       # Tiene que ser el Id del nuevo nodo
